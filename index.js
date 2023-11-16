27372243d4ea6e38fa66e460d0a8922ed176bd63
Carol A. Nikolic <carolnikolic@gmail.com>
2023-11-16 20:29:49 +0100

    chore(deps): installing the project dependencies express and body-parser.

--- index.js
+++ index.js
@@ -1,30 +1,7 @@
-import inquirer from "inquirer";
+import express from "express";
 import qr from "qr-image";
 import fs from "fs";
+import { dirname } from "path";
 import path from "path";
-
-inquirer
-.prompt([{
-    message: "Enter your URL: ",
-    name: "URL" 
-}])
-.then((answers) => {
-    const url = answers.URL;
-    let qr_png = qr.image(url);
-
-    let outputQR = path.join("public/images/outputQR", "qr-img.png")
-
-    qr_png.pipe(fs.createWriteStream(outputQR));
-    
-    fs.writeFile("URL.txt", url, (err) => {
-        if (err) throw err;
-    })
-})
-.catch((error) => {
-    if (error.isTtyError) {
-        console.log("Prompt couldn't be rendered in the current environment.")
-    } else {
-        console.log("Something went wrong.")
-    }
-});
-
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
