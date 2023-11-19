6b1f178045808aa7c2e30773e3b929f955d394aa
Carol A. Nikolic <carolnikolic@gmail.com>
2023-11-19 18:31:32 +0100

    update project

--- index.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import qr from "qr-image";
-import fs from "fs";
-import { dirname } from "path";
-import path from "path";
-import { fileURLToPath } from "url";
-import bodyParser from "body-parser";
